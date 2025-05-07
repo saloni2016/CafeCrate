429b787d8b249f7c0b01ff7f7f284b96809416e9
saloni2016 <149263322+saloni2016@users.noreply.github.com>
2025-05-07 19:37:37 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  //Menu Open Class 
+  let menu = document.querySelector('.menu-icon');
+  let navbar = document.querySelector('.navbar');
+
+  
+  menu.onclick =() => {
+    menu.classList.toggle('move');
+    navbar.classList.toggle('open-menu')
+  };
+  //Menu close on Scroll
+  window.onscrcoll =() => {
+    menu.classList.remove('move');
+    navbar.classList.remove('open-menu');
+  }; 
+  //ScrollReveal Animation
+  const animate = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: '2500',
+    delay:'400',
+  })
+  animate.reveal(".nav");
+  animate.reveal(".home-text", {origin:"left"});
+  animate.reveal(".home-img", {origin:"bototom"});
+  animate.reveal(".ser-box, .product-box, .book-data", {interval:100});
